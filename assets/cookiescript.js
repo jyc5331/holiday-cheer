95b494364a8cf2ee08d82787854da7bb8184aaa7
jyc5331 <jcosgrovecoding@gmail.com>
2020-12-01 11:38:55 -0500

    cookiescript.js linked and running a skeleton function

---
+++ assets/cookiescript.js
@@ -0,0 +1,8 @@
+// develop branch 
+const cookieSearchBtn = document.querySelector('cookieSearchBtn');
+
+const cookieSearch = function(){
+    console.log("running a cookie search now");
+}
+
+cookieSearch();
