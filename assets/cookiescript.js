1ec532a0661b1088b838e201ba52762a031a9ea2
jyc5331 <jcosgrovecoding@gmail.com>
2020-12-01 15:50:10 -0500

    cookie search button functioning

--- assets/cookiescript.js
+++ assets/cookiescript.js
@@ -1,8 +1,8 @@
-// develop branch 
-const cookieSearchBtn = document.querySelector('.cookieSearchBtn');
+// develop branch MAKE SURE YOUR SCREEN WIDTH IS 750PX OR LOWER
+const cookieSearchBtn = document.getElementById("cookieSearchBtn");
 
 const cookieSearch = function(){
+    event.preventDefault();
     console.log("running a cookie search now");
-}
+};  
 
-cookieSearchBtn.addEventListener('click', cookieSearch);
