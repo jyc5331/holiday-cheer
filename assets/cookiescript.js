b00d51526fb9626cf2bbdad89feb3666ad130522
jyc5331 <jcosgrovecoding@gmail.com>
2020-12-01 12:00:24 -0500

    event handler added to cookiescript, not working yet

--- assets/cookiescript.js
+++ assets/cookiescript.js
@@ -1,8 +1,8 @@
 // develop branch 
-const cookieSearchBtn = document.querySelector('cookieSearchBtn');
+const cookieSearchBtn = document.querySelector('.cookieSearchBtn');
 
 const cookieSearch = function(){
     console.log("running a cookie search now");
 }
 
-cookieSearch();
+cookieSearchBtn.addEventListener('click', cookieSearch);
