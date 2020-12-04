78a63d9b0f3cf72226785eb401dc11acf2297df9
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-12-04 13:47:21 -0500

    added functionality to search recipies on cookiescript

--- assets/cookiescript.js
+++ assets/cookiescript.js
@@ -1,34 +1,88 @@
 // develop branch MAKE SURE YOUR SCREEN WIDTH IS 750PX OR LOWER
-const cookieSearchBtn = document.getElementById("cookieSearchBtn");
-const cookieContainer = document.getElementById("cookieContainer");
-const cookieImage = document.getElementById("cookieImage");
-const cookieTitle = document.getElementById("cookieTitle");
-const cookieDescription = document.getElementById("cookieDescription");
 
-//HELP fetch API, should probably be incorporated inside of the cookieSearch function
-fetch('https://api.spoonacular.com/recipes/complexSearch?query=cookies&number=170&apiKey=37ea1e03e0d84b67992ee6276a961a91')
-    .then(cookieResults => cookieResults.json())
-    .then(cookieData => console.log(cookieData))
+var cookieSearchBtn = document.getElementById("cookieSearchBtn");
+var cookieContainer = document.getElementById("cookieContainer");
+
+// var cookieImage = document.getElementById("cookieImage");
+// var cookieTitle = document.getElementById("cookieTitle");
+// var cookieDescription = document.getElementById("cookieDescription");
+
+var cookieInput = document.getElementById("cookie-input");
 
 
-const cookieSearch = function(){
-    event.preventDefault();
-    console.log("running a cookie search now");
+var getSearchTerm = function (event) {
 
-    //generate the card, thoughts on card display: should only be title that links to recipe, a short description, and a picture
-    cookieContainer.innerHTML +=
-    `<div class="card-content m-5">
-    <div class="media">
-       <div class="media-left">
-        <figure class="image is-128x128 card-image" id="cookieImage">
-          <img src="https://bulma.io/images/placeholders/256x256.png" alt="Placeholder image">
-        </figure>
-       </div>
-       <div class="media-content">
-        <p class="title is-4" id="cookieTitle">Recipe Title</p>
-        <p id="cookieDescription"> Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deserunt aperiam, quo ullam nihil ea deleniti, ducimus velit est, voluptas architecto debitis? Alias praesentium autem velit? Voluptatibus eligendi nemo quia?</p>
-       </div>
-    </div>
-</div>`
-};  //end of cookieSearch function
+  event.preventDefault();
 
+  var searchTerm = cookieInput.value
+   
+  getCookieData(searchTerm)
+
+  
+};
+
+
+
+var getCookieData = function (searchTerm) {
+
+  console.log(searchTerm)
+  
+  var cookieDataApiUrl = 'https://api.spoonacular.com/recipes/complexSearch?query='+searchTerm+'+cookies&addRecipeInformation=true&number=170&apiKey=37ea1e03e0d84b67992ee6276a961a91'
+
+    //format the response/promise
+    fetch(cookieDataApiUrl)
+        .then(function(response) {
+          if (response.ok){
+            response.json().then(function(data) {
+
+              // call displayCookieData and pass data as an argument
+              console.log(data);
+              displayCookieData(data);
+              
+            });
+          } else {
+          alert("Error: " + response.statusText);
+          }
+        })
+        .catch(function(error) {
+          alert("unable to connect to server");
+        });
+   
+};
+
+
+var displayCookieData = function(data){
+  
+  // loop through the data and dynamically create the recipie cards 
+
+  for (var i= 0;  i < data.results.length; i++ ) {
+
+    var div = document.createElement("div")
+
+    var innerHtml = 
+
+      '<div class="card-content m-5">'+
+        '<div class="media">' +
+          '<div class="media-left">' +
+          '<figure class="image is-128x128 card-image">' +
+            '<img src="'+ data.results[i].image +'" alt="Placeholder image">'+
+          '</figure>'+
+         '</div>' +
+         '<div class="media-content">'+
+            '<p class="title is-4" id="cookieTitle">'+ data.results[i].title +'</p>'+
+            '<p></p><br>'+
+            '<a href="" >Recipie Link</a>' +
+         '</div>'+
+        '</div>'+
+      '</div>'
+
+      div.innerHTML = innerHtml
+
+      cookieContainer.appendChild(div);
+  }
+ 
+}; 
+
+
+
+cookieSearchBtn.addEventListener("click", getSearchTerm);
