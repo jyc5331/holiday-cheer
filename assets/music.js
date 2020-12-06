abc16172e7da7280e7136b632e1ed723a3dd1f0a
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-12-06 14:00:44 -0500

    made music modal and added song

---
+++ assets/music.js
@@ -0,0 +1,22 @@
+var musicButton = document.getElementById("music-button");
+
+var musicModal = document.getElementById("music-modal");
+
+var closeButton = document.getElementById("close-button");
+
+
+var displayModal = function () {
+
+    musicModal.classList.add("is-active")   
+};
+
+
+var closeModal = function () {
+
+    musicModal.classList.remove("is-active")
+    
+}
+
+musicButton.addEventListener("click", displayModal);
+
+closeButton.addEventListener("click", closeModal);
