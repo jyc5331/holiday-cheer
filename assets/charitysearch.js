55cc1127175e1601a4e53732e334053fdf1ce386
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-12-06 22:58:33 -0500

    data for api fetched, next step is to display in modal

--- assets/charitysearch.js
+++ assets/charitysearch.js
@@ -4,6 +4,8 @@ var charityModal = document.getElementById("openModal");
 
 var closeCharityBtn = document.getElementById("close-button2");
 
+var searchBtn = document.getElementById("site-search");
+
 // Fxn to open modal 
 var openCharityModal = function () {
     openCharityBtn.onclick = function () {
@@ -11,14 +13,52 @@ var openCharityModal = function () {
     }
 };
 
-// Fxn that will fetch charity api data
+// Fxn that will search charity api data
+
+ var searchInput = document.getElementById("search-input");
+
+ searchInput.value = ""
+ 
+ var getSearchInput = function (event) {
+
+    // event.preventDefault();
+  
+    var searchInput = searchInput.value
+     
+    // fetchCharityData(searchInput)
+  
+    // searchInput.value = ""
+
+    event.preventDefault();
+
+    searchBtn.onclick = fetchCharityData();
+    
+ 
+  };
+
+// Fxn that will pass search input as argument through fecthCharityData
+  
+var fetchCharityData = function (searchInput) {
+
+    // console.log(searchTerm)
+
+    var charityDataApiUrl = "http://data.orghunter.com/v1/charitysearch?user_key=b621ed5a3921119ea871a341755aece6&searchTerm=" + searchInput + ""
+
+    fetch(charityDataApiUrl)
+        .then(function(response) {
+          if (response.ok){
+            console.log("fetched data");
+        }
+    }
+
+ )};
 
 // Fxn to populate modal with fetched data
 
 // Fxn to close modal
 closeCharityBtn.onclick = function () {
     charityModal.style.display = "none";
-}
+};
 
 
 
@@ -35,3 +75,4 @@ closeCharityBtn.onclick = function () {
 
 // Event Listeners
 openCharityBtn.addEventListener("click", openCharityModal);
+searchBtn.addEventListener("submit", fetchCharityData);
